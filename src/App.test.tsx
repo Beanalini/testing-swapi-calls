cc34cce11d91bb7c24e149d3b3cb0842392b4af4
Beanalini <dat826@gmail.com>
2023-12-19 14:15:47 +0000

    chore:add MSW as dev dependency

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { setupServer } from "msw/node";
+import { http, HttpResponse } from "msw";
 
-test('renders learn react link', () => {
+const handler = http.get("https://swapi.dev/api/people/1", () => {
+  return HttpResponse.json({ name: "Luke Skywalker" });
+});
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+const server = setupServer(handler);
+
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test("renders the heading h1", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Star Wars:Testing API Calls/i);
+  expect(titleElement).toBeInTheDocument();
 });
